78e39a6ca125348d3748251deb23bb809b580758
Leonardo Nunes <leonunesbs@gmail.com>
2024-05-04 15:25:16 -0300

    feat: remove category scrollbar

--- src/components/organisms/CategoriesBar/CategoriesRoot.tsx
+++ src/components/organisms/CategoriesBar/CategoriesRoot.tsx
@@ -6,7 +6,10 @@ interface CategoriesRootProps {
 
 export function CategoriesRoot({ children }: CategoriesRootProps) {
   return (
-    <div className="bg-primary text-primary-content space-x-2 overflow-scroll justify-start sm:justify-center flex whitespace-nowrap shadow dark:shadow-none">
+    <div
+      className="bg-primary text-primary-content space-x-2 overflow-scroll justify-start sm:justify-center flex whitespace-nowrap shadow dark:shadow-none"
+      style={{ scrollbarWidth: 'none', msOverflowStyle: 'none' }}
+    >
       {children}
     </div>
   );
