1504399354e7811a2fee95500baa6330c0c016f4
Leonardo Nunes <leonunesbs@gmail.com>
2024-05-12 16:56:37 -0300

    fix: adjust viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -109,8 +109,6 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: 'device-width',
   initialScale: 1,
-  maximumScale: 1,
-  userScalable: false,
   themeColor: '#fff',
 
   // Also supported by less commonly used
