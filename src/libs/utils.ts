ce62c23122c12006460fe7245598b6a71643bb45
Leonardo Nunes <leonunesbs@gmail.com>
2024-05-12 21:06:46 -0300

    fix: fix

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -2,7 +2,7 @@ export function normalizeText(text: string) {
   return text
     .normalize('NFD') // Normaliza a string para a forma de decomposição canônica
     .replace(/[\u0300-\u036f]/g, '') // Remove acentos
-    .replace(/[^a-z0-9]/gi, '') // Remove caracteres não alfanuméricos
+    .replace(/[^a-zA-Z0-9]/g, '') // Remove caracteres especiais, mantendo letras e números
     .toLowerCase(); // Converte para minúsculo
 }
 
